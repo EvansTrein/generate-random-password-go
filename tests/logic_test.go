ad02302f16543fb919c12c61137b7cfee33ddb7c
Evans <evanstrein@icloud.com>
2024-09-26 03:05:47 +0300

    add tests

--- tests/logic_test.go
+++ tests/logic_test.go
@@ -6,8 +6,28 @@ import (
 )
 
 
-func TestIsValidPassword(t *testing.T) {
-	fail := []string{"123456", "QWERTY", "qwerty"}
+func TestIsValidPasswordInCorrect(t *testing.T) {
+	fail := []string{"123456", "QWERTY", "qwerty", "qweW123"}
+
+	for _, el := range fail {
+		if logic.IsValidPassword(el) {
+			t.Errorf("Generated password is %s", el)
+		}
+	}
+}
+
+func TestIsValidPasswordСorrect(t *testing.T) {
+	fail := []string{"P!$W5$n_nzOFyK", "0hJuzKH$nUc2eL", "$3nnd92*bT", "H7!vRWtCRI"}
+
+	for _, el := range fail {
+		if !logic.IsValidPassword(el) {
+			t.Errorf("Generated password is %s", el)
+		}
+	}
+}
+
+func TestIsValidPasswordNoSymbols(t *testing.T) {
+	fail := []string{"7WAqIlNszD", "T0BffjQk0P57YnUc2eL", "C5ccRCR", "y0EsF"}
 
 	for _, el := range fail {
 		if logic.IsValidPassword(el) {
